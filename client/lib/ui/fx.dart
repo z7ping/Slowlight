314eb9602dff98f4c8117db1e9c8b15e11437c1a
7ping <z7ping@outlook.com>
2026-08-30 07:01:37 +0800

    chore(ui): 导出 FxSectionHeader

--- client/lib/ui/fx.dart
+++ client/lib/ui/fx.dart
@@ -16,6 +16,7 @@ export 'widgets/fx_progress.dart';
 export 'widgets/fx_badge.dart';
 export 'widgets/fx_tab.dart';
 export 'widgets/fx_page_header.dart';
+export 'widgets/fx_section_header.dart';
 export 'widgets/section_title.dart';
 
 export 'widgets/fx_date_time_picker.dart';
